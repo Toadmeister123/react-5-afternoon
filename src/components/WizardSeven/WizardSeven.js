46472f3ce78f6f7260c1202ceaf09b4a9da9ee40
Toadmeister123 <todd.meister99@gmail.com>
2019-02-18 19:03:37 -0700

    initial commit

--- src/components/WizardSeven/WizardSeven.js
+++ src/components/WizardSeven/WizardSeven.js
@@ -1,9 +1,12 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+import { updateDownPayment } from '../../ducks/reducer'
 
 class WizardSeven extends Component {
 
     render(){
+        const { updateDownPayment } = this.props
         return(
             <div className="parent-div">
                 <div className="vert-align">                    
@@ -11,10 +14,10 @@ class WizardSeven extends Component {
                     <p>Estimate your credit score</p> <br />
                     
                     <div className="row">
-                        <Link to="/wEight"><button onClick={this.props.updateCreditE}>Excellent</button></Link>
-                        <Link to="/wEight"><button onClick={this.props.updateCreditG}>Good</button></Link>
-                        <Link to="/wEight"><button onClick={this.props.updateCreditF}>Fair</button></Link>
-                        <Link to="/wEight"><button onClick={this.props.updateCreditP}>Poor</button></Link>
+                        <Link to="/wEight"><button onClick={(e) => updateDownPayment( e.target.value )}>Excellent</button></Link>
+                        <Link to="/wEight"><button onClick={(e) => updateDownPayment( e.target.value )}>Good</button></Link>
+                        <Link to="/wEight"><button onClick={(e) => updateDownPayment( e.target.value )}>Fair</button></Link>
+                        <Link to="/wEight"><button onClick={(e) => updateDownPayment( e.target.value )}>Poor</button></Link>
                     </div>
                 </div>
             </div>
@@ -22,4 +25,11 @@ class WizardSeven extends Component {
     }
 }
 
-export default WizardSeven;
+function mapStateToProps( state ){
+    const { downPayment } = state 
+
+    return {
+        downPayment
+    }
+}
+export default connect(mapStateToProps, { updateDownPayment })(WizardSeven);
